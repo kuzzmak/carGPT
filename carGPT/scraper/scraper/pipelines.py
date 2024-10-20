92ca4e3716e626d0e2ca26fe88db96ea13204af4
kuzzmak <kuzminski.antonio@gmail.com>
2024-10-20 12:52:01 +0200

    Pipeline for inserting car ad data into database

--- carGPT/scraper/scraper/pipelines.py
+++ carGPT/scraper/scraper/pipelines.py
@@ -7,7 +7,25 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+from carGPT.database.database import Database
+
 
 class ScraperPipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class DatabasePipeline:
+
+    def open_spider(self, spider):
+        db = Database()
+        self.db = db.db
+        
+    def process_item(self, item, spider):
+        keys = list(item.keys())
+        keys_str = ', '.join(keys)
+        vals = list(item.values())
+        vals_str = ', '.join([f'"{val}"' for val in vals])
+        insert_query = f'INSERT INTO articles ({keys_str}) VALUES ({vals_str})'
+        self.db.run(insert_query)
         return item
