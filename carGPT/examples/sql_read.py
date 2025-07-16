2441862be04170315b0ea210f0d6b675c3f49d89
kuzzmak <kuzminski.antonio@gmail.com>
2025-07-16 21:15:26 +0200

    New version

--- carGPT/examples/sql_read.py
+++ carGPT/examples/sql_read.py
@@ -1,13 +1,22 @@
 import os
 os.environ["OPENAI_API_KEY"] = "..."
 
-from langchain_community.agent_toolkits import create_sql_agent
+from langchain.chains import create_sql_query_chain
 from langchain_community.utilities import SQLDatabase
 from langchain_openai import ChatOpenAI
 
 
 if __name__ == '__main__':
-    db = SQLDatabase.from_uri("sqlite:///articles.db")
+    connection_string = (
+        "postgresql+psycopg2://adsuser:pass@localhost:5432/ads_db"
+    )
+    db = SQLDatabase.from_uri(connection_string)
     llm = ChatOpenAI(model="gpt-4o", temperature=0)
-    agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
-    res = agent_executor.invoke("list all toyota cars from the articles that are newer than 2020")
+    chain = create_sql_query_chain(llm, db)
+    # res = agent_executor.invoke("list all toyota cars from the articles that are newer than 2020")
+    response = chain.invoke(
+        {
+            "question": "list all toyota cars from the articles that are newer than 2020"
+        }
+    )
+    print(response)
