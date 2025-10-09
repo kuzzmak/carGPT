20d3d8b03bcb7986c3466ba74921381194f792a2
kuzzmak <kuzminski.antonio@gmail.com>
2025-10-09 21:11:20 +0200

    Add paths for scripts directory and Tor browser launcher

--- scraper/src/scraper/paths.py
+++ scraper/src/scraper/paths.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
 SCRAPER_DIR = Path(__file__).parent.parent.parent
+SCRIPTS_DIR = SCRAPER_DIR / "scripts"
+TOR_PATH = SCRAPER_DIR / "_browser" / "tor-browser" / "Browser" / "start-tor-browser"
